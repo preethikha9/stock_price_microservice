098cd5b03b4a247581d3e943f96f9c981cf024cd
Preethikha <43961271+preethikha9@users.noreply.github.com>
2024-03-10 17:17:54 +0530

    Update GetStockPrice_tests.py

--- src/unittest/python/GetStockPrice_tests.py
+++ src/unittest/python/GetStockPrice_tests.py
@@ -1,36 +1,44 @@
-import unittest
-import requests_mock
+"""
+GetStockPRice_tests 
+"""
+
+import unittest, requests_mock
 from GetStockPrice import app
+
 
 class TestGetStockPrice(unittest.TestCase):
     """
-    test
+    TestGetStockPrice
     """
+
     def test_get_stock_price_success(self):
-            """
-            test
-            """
-        symbol = 'AAPL'
-        expected_price = '170.73'
+        """
+        test_get_stock_price_success ()
+        """
+        symbol = "AAPL"
+        expected_price = "170.73"
         with requests_mock.Mocker() as mocker:
-            mocker.get(f"https://finance.yahoo.com/quote/{symbol}", text='<span>Current</span><span>170.73</span>')
-            response = app.test_client().get(f'/stock/{symbol}')
+            url = f"https://finance.yahoo.com/quote/{symbol}"
+            text = "<span>Current</span><span>170.73</span>"
+            x = mocker.get(url, text=text)
+            response = app.test_client().get(f"/stock/{symbol}")
             data = response.get_json()
             self.assertEqual(response.status_code, 200)
-            self.assertEqual(data['symbol'], symbol)
-            self.assertEqual(data['price'], expected_price)
+            self.assertEqual(data["symbol"], symbol)
+            self.assertEqual(data["price"], expected_price)
 
     def test_get_stock_price_failure(self):
-            """
-            test
-            """
-        symbol = 'AAPL'
+        """
+        test_get_stock_price_failure
+        """
+        symbol = "AAPL"
         with requests_mock.Mocker() as mocker:
             mocker.get(f"https://finance.yahoo.com/quote/{symbol}", status_code=404)
-            response = app.test_client().get(f'/stock/{symbol}')
+            response = app.test_client().get(f"/stock/{symbol}")
             data = response.get_json()
             self.assertEqual(response.status_code, 500)
-            self.assertEqual(data['error'], 'Failed to fetch stock price')
+            self.assertEqual(data["error"], "Failed to fetch stock price")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
