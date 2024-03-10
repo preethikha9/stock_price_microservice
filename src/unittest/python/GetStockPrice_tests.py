636940674b53118e209312029415ed2bfec17244
Preethikha <43961271+preethikha9@users.noreply.github.com>
2024-03-10 20:46:21 +0530

    build working

--- src/unittest/python/GetStockPrice_tests.py
+++ src/unittest/python/GetStockPrice_tests.py
@@ -1,44 +1,33 @@
-"""
-GetStockPRice_tests 
-"""
+import unittest
+from unittest.mock import patch
+import requests_mock
+from bs4 import BeautifulSoup
+from get_stock_price import app  # Import the Flask app
 
-import unittest, requests_mock
-from get_stock_price import app
+class GetStockPriceTest(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()  # Create test client from the Flask app
 
-
-class TestGetStockPrice(unittest.TestCase):
-    """
-    TestGetStockPrice
-    """
-
-    def test_get_stock_price_success(self):
-        """
-        test_get_stock_price_success ()
-        """
-        symbol = "AAPl"
-        expected_price = "170.73"
+    def test_get_stock_price(self):
+        symbol = 'AAPL'
+        expected_price = '170.73'
         with requests_mock.Mocker() as mocker:
-            url = f"https://finance.yahoo.com/quote/{symbol}"
-            text = "<span>Current</span><span>170.73</span>"
-            x = mocker.get(url, text=text)
-            response = app.test_client().get(f"/stock/{symbol}")
+            mocker.get(f"https://finance.yahoo.com/quote/{symbol}", text='<span>Current</span><span>170.73</span>')
+            response = self.client.get(f'/stock/{symbol}')
             data = response.get_json()
             self.assertEqual(response.status_code, 200)
-            self.assertEqual(data["symbol"], symbol)
-            #self.assertEqual(data["price"], expected_price)
-    '''
+            self.assertEqual(data['symbol'], symbol)
+            self.assertEqual(data['price'], expected_price)
+
+    
     def test_get_stock_price_failure(self):
-        """
-        test_get_stock_price_failure
-        """
-        symbol = "AAPl"
+        symbol = 'AAPL'
         with requests_mock.Mocker() as mocker:
             mocker.get(f"https://finance.yahoo.com/quote/{symbol}", status_code=404)
-            response = app.test_client().get(f"/stock/{symbol}")
+            response = self.client.get(f'/stock/{symbol}')
             data = response.get_json()
             self.assertEqual(response.status_code, 500)
-            self.assertEqual(data["error"], "Failed to fetch stock price")
-    '''
-
-if __name__ == "__main__":
+            self.assertEqual(data['error'], 'Failed to fetch stock price')
+    
+if __name__ == '__main__':
     unittest.main()
